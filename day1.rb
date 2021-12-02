5f3f45d94e593613f6478548b31661829ff00fd6
Elraine <contact@alexischan.me>
2021-12-02 22:29:58 +0100

    cleanup day1

--- day1.rb
+++ day1.rb
@@ -1,22 +1,20 @@
 def sol1()
-    count = 0
     source = File.open("day1.txt")
-    content = source.readlines.map(&:to_i).each_cons(2){
+    content = source.readlines.map(&:to_i).each_cons(2).count{
         |tuple|
-        (tuple[0] < tuple[1]) ? count +=1 : count
+        (tuple[0] < tuple[1])
     }
-    puts "day1 sol1 #{count}"
+    puts "day1 sol1 #{content}"
 end
 
 
 def sol2()
-    count = 0
     source = File.open("day1.txt")
-    content = source.readlines.map(&:to_i).each_cons(4){
+    content = source.readlines.map(&:to_i).each_cons(4).count{
         |tuple|
-        (tuple[0..2].sum < tuple[1..3].sum) ? count += 1 : count
+        (tuple[0..2].sum < tuple[1..3].sum)
     }
-    puts "day1 sol2 #{count}"
+    puts "day1 sol2 #{content}"
 end
 
 sol1()
